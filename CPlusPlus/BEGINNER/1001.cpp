f84c1f74b2d61c8f9f964bf7f982cf459606c342
Marcelo Hadad <marcelo.hadad@outlook.com>
2022-07-10 02:48:08 -0300

    :sparkles: Cria diretório para o cpp e adiciona dois exercícios.

---
+++ CPlusPlus/BEGINNER/1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int A = 0;
+    int B = 0;
+    int X = 0;
+
+    std::cin >> A >> B;
+
+    X = A + B;
+
+    std::cout << "X = " << X << std::endl;
+
+    return 0;
+}
